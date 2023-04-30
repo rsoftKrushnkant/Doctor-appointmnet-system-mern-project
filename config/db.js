fd4149b4b2c9263e8ed641f031bf9e36b4865eb8
Shaikh M Rais <Rsoft.KrushnkantTeam@gmail.com>
2023-04-30 14:05:28 -0400

    update

--- config/db.js
+++ config/db.js
@@ -3,6 +3,7 @@ const colors = require("colors");
 
 const connectDB = async () => {
   try {
+    console.log(`Mongodb connected ${mongoose.connection.host}`.bgGreen.white);
     await mongoose.connect(process.env.MONGO_URL);
     console.log(`Mongodb connected ${mongoose.connection.host}`.bgGreen.white);
   } catch (error) {
